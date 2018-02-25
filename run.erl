485404698008a9c881cdb82fc3f2d1167c34dc83
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2018-02-25 16:14:08 +0300

    Added auto filling of server by client

--- run.erl
+++ run.erl
@@ -1,13 +1,13 @@
 -module(run).
 
 -import(server, [start/1, slink/1]).
--import(client, [cstart/1]).
+-import(client, [cstart/1, set/2]).
 
--export([server/1, client/1]).
+-export([server/1, client/1, cfill/0]).
 
 client(ServerNameList) -> 
     [ServerName | _T] = ServerNameList,
-    cstart(ServerName).
+    cstart(list_to_atom(ServerName)).
 
 server(Args) -> 
     [NumberString | NeighborNames] = Args,
@@ -17,6 +17,12 @@ server(Args) ->
         false -> linkNeighbors(NeighborNames);
         true  -> ok
     end.
+
+cfill() ->
+    set(alice, amsterdam),
+    set(bob, berlin),
+    set(carol, coppenhagen),
+    set(derek, dubai).
 
 linkNeighbors(_NeighborNames = []) -> ok;
 linkNeighbors(_NeighborNames = [H | T]) ->
