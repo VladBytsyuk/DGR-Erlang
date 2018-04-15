230bad3ed833c109fd9e483765555f707a13b8eb
Vladislav Bytsyuk <vladislav.bytsyuk@gmail.com>
2018-04-15 12:20:16 +0300

    Added measurements

--- output.sh
+++ output.sh
@@ -1,23 +1,23 @@
 gnome-terminal -e "erl -sname 'barrier@vbytsyuk-xps15' -run dgr start_barrier"
 
 gnome-terminal -e "erl -sname 'alice@vbytsyuk-xps15' -run run server 'a' 3 'barrier@vbytsyuk-xps15'"
-gnome-terminal -e "erl -sname 'client_alice@vbytsyuk-xps15' -run run client 'alice@vbytsyuk-xps15'"
+gnome-terminal -e "erl -sname 'client_alice@vbytsyuk-xps15' -run run client 'alice@vbytsyuk-xps15' 1 20"
 
-gnome-terminal -e "erl -sname 'bob@vbytsyuk-xps15' -run run server 'b' 3 'barrier@vbytsyuk-xps15'"
-gnome-terminal -e "erl -sname 'client_bob@vbytsyuk-xps15' -run run client 'bob@vbytsyuk-xps15'"
+gnome-terminal -e "erl -sname 'bob@vbytsyuk-xps15' -run run server 'b' 3 'barrier@vbytsyuk-xps15' 'alice@vbytsyuk-xps15'"
+gnome-terminal -e "erl -sname 'client_bob@vbytsyuk-xps15' -run run client 'bob@vbytsyuk-xps15' 21 40"
 
 gnome-terminal -e "erl -sname 'carol@vbytsyuk-xps15' -run run server 'c' 3 'barrier@vbytsyuk-xps15' 'alice@vbytsyuk-xps15'"
-gnome-terminal -e "erl -sname 'client_carol@vbytsyuk-xps15' -run run client 'carol@vbytsyuk-xps15'"
+gnome-terminal -e "erl -sname 'client_carol@vbytsyuk-xps15' -run run client 'carol@vbytsyuk-xps15' 41 60"
 
-gnome-terminal -e "erl -sname 'derek@vbytsyuk-xps15' -run run server 'd' 3 'barrier@vbytsyuk-xps15'"
-gnome-terminal -e "erl -sname 'client_derek@vbytsyuk-xps15' -run run client 'derek@vbytsyuk-xps15'"
+gnome-terminal -e "erl -sname 'derek@vbytsyuk-xps15' -run run server 'd' 3 'barrier@vbytsyuk-xps15' 'carol@vbytsyuk-xps15'"
+gnome-terminal -e "erl -sname 'client_derek@vbytsyuk-xps15' -run run client 'derek@vbytsyuk-xps15' 61 80"
 
 gnome-terminal -e "erl -sname 'erik@vbytsyuk-xps15' -run run server 'e' 3 'barrier@vbytsyuk-xps15' 'bob@vbytsyuk-xps15' 'derek@vbytsyuk-xps15'"
-gnome-terminal -e "erl -sname 'client_erik@vbytsyuk-xps15' -run run client 'erik@vbytsyuk-xps15'"
+gnome-terminal -e "erl -sname 'client_erik@vbytsyuk-xps15' -run run client 'erik@vbytsyuk-xps15' 81 100"
 
-gnome-terminal -e "erl -sname 'fred@vbytsyuk-xps15' -run run server 'f' 3 'barrier@vbytsyuk-xps15' 'carol@vbytsyuk-xps15'"
-gnome-terminal -e "erl -sname 'client_fred@vbytsyuk-xps15' -run run client 'fred@vbytsyuk-xps15'"
+gnome-terminal -e "erl -sname 'fred@vbytsyuk-xps15' -run run server 'f' 3 'barrier@vbytsyuk-xps15' 'carol@vbytsyuk-xps15' 'erik@vbytsyuk-xps15'"
+gnome-terminal -e "erl -sname 'client_fred@vbytsyuk-xps15' -run run client 'fred@vbytsyuk-xps15' 101 120"
 
 gnome-terminal -e "erl -sname 'george@vbytsyuk-xps15' -run run server 'g' 3 'barrier@vbytsyuk-xps15' 'derek@vbytsyuk-xps15'"
-gnome-terminal -e "erl -sname 'client_george@vbytsyuk-xps15' -run run client 'george@vbytsyuk-xps15'"
+gnome-terminal -e "erl -sname 'client_george@vbytsyuk-xps15' -run run client 'george@vbytsyuk-xps15' 121 140"
 
--- run.erl
+++ run.erl
@@ -3,15 +3,17 @@
 -import(server, [start/3, slink/1]).
 -import(client, [cstart/1, set/2]).
 
--export([server/1, client/1, cfill/0, http/1]).
+-export([server/1, client/1, cfill/2, measure/1, measure/2, getObjectNTimes/2, http/1]).
 
-client(ServerNameList) -> 
-    [ServerName | _T] = ServerNameList,
-    cstart(list_to_atom(ServerName)).
+client(Args) -> 
+    [ServerName, LeftString, RightString | _T] = Args,
+    {Left, _} = string:to_integer(LeftString),
+    {Right, _} = string:to_integer(RightString),
+    cstart(list_to_atom(ServerName)),
+    cfill(Left, Right).
 
 server(Args) -> 
     [NumberString, CapacityString, BarrierNode | NeighborNames] = Args,
-    %{Number, _} = string:to_integer(NumberString),
     Number = list_to_atom(NumberString),
     {Capacity, _} = string:to_integer(CapacityString),
     start(Number, Capacity, {barrier, list_to_atom(BarrierNode)}),
@@ -20,15 +22,30 @@ server(Args) ->
         true  -> ok
     end.
 
-cfill() ->
-    set(alice, amsterdam),
-    set(bob, berlin),
-    set(carol, coppenhagen),
-    set(derek, dubai).
-
+cfill(A, B) when A > B -> cfill(B, A);
+cfill(B, B) -> 
+    client:set(B, B),
+    {ok, fill};
+cfill(A, B) -> 
+    client:set(A, A),
+    cfill(A + 1, B).
+    
 linkNeighbors(_NeighborNames = []) -> ok;
 linkNeighbors(_NeighborNames = [H | T]) ->
     slink(list_to_atom(H)),
     linkNeighbors(T).
+
+getObjectNTimes(1, Key) -> client:get(Key);
+getObjectNTimes(N, Key) ->
+    client:get(Key),
+    getObjectNTimes(N - 1, Key).
+
+measure(Key) ->
+    measure(1000, Key).
+
+measure(N, Key) ->
+    {Time, Object} = timer:tc(run, getObjectNTimes, [N, Key]),
+    {{time, Time / N / 1000000}, {object, Object}}.
+    
 
 http(Port) -> http_server:start(Port).
